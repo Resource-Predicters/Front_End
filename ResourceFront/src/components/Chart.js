fd4aa17b3dcbb3fc71bc45a098983f31cea90a10
JoonGit <ljj9730@gmail.com>
2023-10-30 22:34:27 +0900

    Update Chart.js

--- ResourceFront/src/components/Chart.js
+++ ResourceFront/src/components/Chart.js
@@ -5,7 +5,6 @@ function Chart(props) {
   const [chartData, setChartData] = useState(null);
   useEffect(() => {
     // 데이터 포인트마다 색상을 설정하는 새로운 데이터 배열 생성
-
     const dataWithColors = props.price.map((price, index) => ({
       x: props.date[index],
       y: price,
@@ -13,6 +12,58 @@ function Chart(props) {
     }));
 
     // 그래프 데이터 및 옵션 초기화
+    const chartOptions = {
+      chart: {
+        height: 300,
+        type: "line",
+        zoom: {
+          type: "x",
+          enabled: true,
+          autoScaleYaxis: true,
+        },
+        toolbar: {
+          autoSelected: "zoom",
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "smooth", // 수정: curve 옵션 변경
+        },
+        title: {
+          text: props.korName + " " + props.unit,
+          align: "center",
+          style: {
+            fontSize: "30px",
+            fontWeight: "bold",
+            fontFamily: undefined,
+            color: props.color, // 그래프 제목 색상
+          },
+        },
+        grid: {
+          row: {
+            colors: [props.color, "transparent"], // 그래프 행 색상
+            opacity: 0.5,
+          },
+        },
+        xaxis: {
+          categories: props.date,
+        },
+        yaxis: {
+          title: {
+            text: props.unit,
+          },
+        },
+      },
+    };
+
+    // forecastDataPoints가 props로 전달된 경우에만 추가
+    if (props.forecastDataPoints) {
+      chartOptions.forecastDataPoints = {
+        count: props.forecastDataPoints,
+      };
+    }
+
     setChartData({
       series: [
         {
@@ -20,52 +71,9 @@ function Chart(props) {
           data: dataWithColors, // 데이터 포인트에 색상이 포함됨
         },
       ],
-      options: {
-        chart: {
-          height: 300,
-          type: "line",
-          zoom: {
-            type: "x",
-            enabled: true,
-            autoScaleYaxis: true,
-          },
-          toolbar: {
-            autoSelected: "zoom",
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          stroke: {
-            curve: "smooth", // 수정: curve 옵션 변경
-          },
-          title: {
-            text: props.korName + " " + props.unit,
-            align: "center",
-            style: {
-              fontSize: "30px",
-              fontWeight: "bold",
-              fontFamily: undefined,
-              color: props.color, // 그래프 제목 색상
-            },
-          },
-          grid: {
-            row: {
-              colors: [props.color, "transparent"], // 그래프 행 색상
-              opacity: 0.5,
-            },
-          },
-          xaxis: {
-            categories: props.date,
-          },
-          yaxis: {
-            title: {
-              text: props.unit,
-            },
-          },
-        },
-      },
+      options: chartOptions,
     });
-  }, [props.price, props.date, props.color]);
+  }, [props.price, props.date, props.color, props.forecastDataPoints]);
 
   return (
     <div id="chart">
